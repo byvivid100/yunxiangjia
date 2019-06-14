c5053e34255cd5bd3f48b9e8cdfb97d463af9b3a
yangsixue <695193756@qq.com>
2019-06-14 18:21:09 +0800

    2

---
+++ application/api/controller/Sms.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/6/13
+ * Time: 10:38
+ */
+
+namespace app\api\controller;
+
+
+class sms
+{
+
+}
---
+++ application/api/model/Config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/6/13
+ * Time: 11:56
+ */
+
+namespace app\api\model;
+
+
+class config
+{
+
+}
