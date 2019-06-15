509557fb0c2368a04c6935e72e1ea5b5f6129404
yangsixue <695193756@qq.com>
2019-06-15 11:24:04 +0800

    接入腾讯短信

--- application/api/controller/Sms.php
+++ application/api/controller/Sms.php
@@ -21,12 +21,11 @@ class Sms extends Controller
         if(!$phone){
             return false;
         }
-        $MaxCount=model('Config')->getConfig('sms_num');
         $valCode=random_int(10000,99999);
         $params=['code'=>$valCode,'phone'=>$phone];
-        $code= model('Sms')->sendSms('249797',$params);
-        $cache = set($code, 'sms_code'.$phone);
-        \app\api\model\Sms::create(['code'=>'12345','phone'=>$phone,'ip'=>request()->ip(),'type'=>$type]);
+        $code= model('Sms')->sendSms($valCode,$params);
+//        $cache = set($code, 'sms_code'.$phone);
+        \app\api\model\Sms::create(['code'=>$valCode,'phone'=>$phone,'ip'=>request()->ip(),'type'=>$type]);
         return Code::send(200,$code);
     }
 }
